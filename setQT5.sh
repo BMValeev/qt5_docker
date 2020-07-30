8d224ebdd04f6791dfbb52080340d301625c606d
Valeev Bulat <vbm@eleps.ru>
2020-07-30 09:29:26 +0300

    little_correction

--- setQT5.sh
+++ setQT5.sh
@@ -2,11 +2,13 @@
 
 cd /home/deploy/qt5
 X1=$(git rev-parse --abbrev-ref HEAD)
-if [[ $# -eq 0 ]]; then
+if [[ $# -eq 0 ]]
+then
     echo -e "No parameters found. "
     exit 1
 fi
-if [[ $1 -ne $X1 ]] then
+if [[ $1 -ne $X1 ]] 
+then
 	git checkout $1
 	perl init-repository -f --branch
 fi
